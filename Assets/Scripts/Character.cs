2455d8fbe674010fd8bbefa50113da37d4e527fb
EyoGO <gentashyura@gmail.com>
2020-05-30 16:35:01 +0300

    Added Scripts folder

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    [SerializeField]
+    private int lives = 3;
+    [SerializeField]
+    private float speed = 2.0f;
+    [SerializeField]
+    private float jumpForce = 15.0f;
+
+    private Rigidbody2D rigidbody;
+    private Animator animator;
+    private SpriteRenderer sprite;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+
+    }
+
+    private void Run()
+    {
+
+    }
+}
