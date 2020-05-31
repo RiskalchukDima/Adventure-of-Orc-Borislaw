c3cb61aaf61c798498ebfd300fc3b07f5511d47f
EyoGO <gentashyura@gmail.com>
2020-05-31 18:08:17 +0300

    Pause menu added

---
+++ Assets/Scripts/UI/PauseControl.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseControl : MonoBehaviour
+{
+    public static bool IS_GAME_PAUSED = false;
+
+    public GameObject pauseMenu;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (IS_GAME_PAUSED)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1.0f;
+        IS_GAME_PAUSED = false;
+    }
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        IS_GAME_PAUSED = true;
+    }
+
+    public void ReturnToMenu()
+    {
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
