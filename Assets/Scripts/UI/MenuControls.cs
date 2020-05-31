f4ebb8e956915a166f8261b1634d95f141ec6d3a
EyoGO <gentashyura@gmail.com>
2020-05-31 16:17:21 +0300

    UI added

---
+++ Assets/Scripts/UI/MenuControls.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuControls : MonoBehaviour
+{
+    public void StartPressed()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+    public void OptionsPressed()
+    {
+    }
+    public void ExitPressed()
+    {
+        Application.Quit();
+    }
+}
